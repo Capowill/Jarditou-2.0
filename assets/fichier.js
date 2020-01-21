3b7a6a6536056092ec808a53286be82f8d3bdf68
80010-11-12 <80010-11-12@exchange.ad.afpanet>
2020-01-21 15:59:46 +0100

    Jarditou en PHP

---
+++ assets/fichier.js
@@ -0,0 +1,100 @@
+// Exercice 7 JS eval
+
+
+
+/* ADRESSE MAIL  */
+function testmail() {
+    // let filtre = /[@]{1}/; mode no friendly
+    let filtre = /^[^\W][a-zA-Z0-9_]+(\.[a-zA-Z0-9_]+)*\@[a-zA-Z0-9_]+(\.[a-zA-Z0-9_]+)*\.[a-zA-Z]{2,4}$/;
+    let mail = document.getElementById("email").value;
+    let result = filtre.test(mail);
+    let errormail = document.getElementById("errorMail");
+    if (!result) {
+        event.preventDefault();
+        console.log("Email non valide, comprenant au moins '@'");
+        errormail.innerHTML = "Champs incorrect *";
+        errormail.style.color = "red";
+    } else {
+        console.log(mail);
+        errormail.innerHTML = "Champs valide";
+        errormail.style.color = "green";
+    }
+}
+
+/* CODE POSTAL */
+function testcode() {
+    let filtre = /^[0-9]{5}$/;
+    let code = document.getElementById("code").value;
+    let result = filtre.test(code);
+    let errorpost = document.getElementById("errorPostal");
+    if (!result) {
+        event.preventDefault();
+        console.log("Code postal incorrect !");
+        errorpost.innerHTML = "Champs obligatoire *";
+        errorpost.style.color = "red";
+    } else {
+        console.log(code);
+        errorpost.innerHTML = "Champs valide";
+        errorpost.style.color = "green";
+    }
+}
+
+/* NOM */
+function testnom() {
+        let filtre = /[a-zA-Z\â\ä\ê\é\è\-\ï\ë\î\ô\ö\û\ü]+$/;
+    let nom = document.getElementById("nom").value;
+    let result = filtre.test(nom);
+    let errorNom = document.getElementById("errorNom");
+    if (!result) {
+        event.preventDefault();
+        console.log("Nom  non valide !");
+        errorNom.innerHTML = "Champs incorrect *";
+        errorNom.style.color = "red";
+    } else {
+        console.log(nom);
+        errorNom.innerHTML = "Champs valide";
+        errorNom.style.color = "green";
+    }
+}
+
+/*  PRENOM */
+function testprenom() {
+    let filtre = /[a-zA-Z\â\ä\ê\é\è\-\ï\ë\î\ô\ö\û\ü]+$/;
+    let prenom = document.getElementById("prenom").value;
+    let result = filtre.test(prenom);
+    let errorPrenom = document.getElementById("errorPrenom");
+    if (!result) {
+        event.preventDefault();
+        console.log(" Prenom non valide !");
+        errorPrenom.innerHTML = "Champs incorrect *";
+        errorPrenom.style.color = "red";
+    } else {
+        console.log(prenom);
+        errorPrenom.innerHTML = "Champs valide";
+        errorPrenom.style.color = "green";
+    }
+}
+
+function testdate(){
+    let filtre = /^[0-9]{4}\-[0-9]{2}\-[0-9]{2}$/; // resultat 00/00/0000
+    let naissance = document.getElementById("naissance").value;
+    let result = filtre.test(naissance);
+    let errorDate = document.getElementById("errordate");
+    if (!result) {
+        event.preventDefault();
+        console.log(naissance);
+        console.log("Date de naissance non valide !");
+        errorDate.innerHTML = "Champs incorrect *";
+        errorDate.style.color = "red";
+    } else {
+        console.log(naissance);
+        errorDate.innerHTML = "Champs valide";
+        errorDate.style.color = "green";
+    }
+}
+
+document.getElementById("submit").addEventListener("click", testdate);
+document.getElementById("submit").addEventListener("click", testnom);
+document.getElementById("submit").addEventListener("click", testprenom);
+document.getElementById("submit").addEventListener("click", testcode);
+document.getElementById("submit").addEventListener("click", testmail);
---
+++ contact.php
@@ -0,0 +1,173 @@
+
+<?php
+
+echo"<br>";
+var_dump($_POST);
+echo"<br>";
+echo"<br>";
+echo"<br>";
+echo"<br>";
+echo"<br>";
+echo"<br>";
+
+
+
+?>
+
+
+<?php
+/* Info formulaire coté serveur V1.0 */
+$prenom = $_POST['prenom'];  
+$nom = $_POST['nom'];
+$adresse =$_POST['Adresse'];
+$ville  = $_POST['Ville'];
+$postal  = $_POST['Code'];
+$naissance = $_POST['Naissance'];
+$mail = $_POST['mail'];
+$sex = $_POST['sex'];
+$sujet = $_POST['Sujet'];
+$com = $_POST['commentaire'];
+
+
+echo " Nom : ".$nom ."<br>";
+echo " Prenom : ".$prenom ."<br>";
+echo " adresse : ".$adresse."<br>";
+echo " ville : ".$ville."<br>";
+echo " Code postal : ".$postal."<br>";
+echo " Date de naissance : " .$naissance."<br>";
+echo " Sexe :" .$sex."<br>";
+echo " Email : ".$mail."<br>";
+echo " Sujet : ".$sujet."<br>";
+echo " Commentaire : ".$com."<br>";
+?>
+
+
+<?php
+echo"<br>";
+echo"<br>";
+echo"<br>";
+    /* =============================================  */
+?>
+
+<?php
+
+/* V2.0 */
+
+//Vérification du nom ====== $nom = $_POST['nom'];
+
+if (empty($_POST["nom"]))
+{
+    $nomErr = "Le nom est requis";
+} else if (!preg_match("/[a-zA-Z\â\ä\ê\é\è\-\ï\ë\î\ô\ö\û\ü]+$/", $nom))
+{
+    $nomErr = "Le nom doit comporter des lettres";
+}else
+{
+    $nomErr = "Valide ! ".$nom;
+}
+echo $nomErr."<br>";
+
+
+// Verification du prenom ====== $prenom = $_POST['prenom']; 
+
+if (empty($_POST["prenom"]))
+{
+    $prenomErr = "Le prenom est requis";
+} else if (!preg_match("/[a-zA-Z\â\ä\ê\é\è\-\ï\ë\î\ô\ö\û\ü]+$/", $prenom))
+{
+    $prenomErr = "Le prenom doit comporter des lettres";
+}else
+{
+    $prenomErr = "Valide ! ".$prenom;
+}
+echo $prenomErr."<br>";
+
+
+// verfi date de naissance ====== $naissance = $_POST['Naissance'];
+
+if (empty($_POST["Naissance"]))
+{
+    $dateErr = " La date de naissance est requis";
+} else if (!preg_match("/^[0-9]{4}\-[0-9]{2}\-[0-9]{2}$/", $naissance))
+{
+    $dateErr = "Date de naissance Incorrect";
+}else
+{
+    $dateErr = "Valide ! ".$naissance;
+}
+echo $dateErr."<br>";
+
+
+// verif adresse ====== $adresse =$_POST['Adresse'];
+
+if (empty($_POST["Adresse"]))
+{
+    $adresseErr = "L'adresse est requis";
+}
+else
+{
+    $adresseErr = "Valide ! ".$adresse;
+}
+echo $adresseErr."<br>";
+
+// verif code postal ====== $postal  = $_POST['Code'];
+
+if (empty($_POST['Code']))
+{
+    $codeErr = "Le Code postal est requis";
+}
+else if (!preg_match("/^[0-9]{5}$/", $postal))
+{
+    $codeErr = "Code postal Incorrect";
+}
+else
+{
+    $codeErr = "Valide ! ".$postal;
+}
+echo $codeErr."<br>";
+
+
+//verif Ville ====== $ville  = $_POST['Ville'];
+
+if (empty($_POST["Ville"]))
+{
+    $villeErr = "La ville est requis";
+}
+else
+{
+    $villeErr = "Valide ! ".$ville;
+}
+echo $villeErr."<br>";
+
+// verif adresse mail ====== $mail = $_POST['mail'];
+
+if (empty($_POST["mail"]))
+{
+    $mailErr = "Le mail est requis";
+} else if (!preg_match("/^[^\W][a-zA-Z0-9_]+(\.[a-zA-Z0-9_]+)*\@[a-zA-Z0-9_]+(\.[a-zA-Z0-9_]+)*\.[a-zA-Z]{2,4}$/", $mail))
+{
+    $mailErr = "E_mail Incorrect";
+}else
+{
+    $mailErr = "Valide ! ".$mail;
+}
+echo $mailErr."<br>";
+
+
+//verif "sexe ====== $sex = $_POST['sex'];
+
+if (empty($_POST["sex"]))
+{
+    $sexErr = "Le sexe est requis";
+}
+else
+{
+    $sexErr = "Valide ! ".$sex;
+}
+echo $sexErr."<br>";
+
+
+
+?>
+
+
